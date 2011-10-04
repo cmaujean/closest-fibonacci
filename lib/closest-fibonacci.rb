43549304de99833a7ac6956508b1c1cdff60e4f1
Christopher Maujean <cmaujean@gmail.com>
2011-10-03 21:25:44 -0700

    one more little refactoring spree
    * in closest_fibonacci, don't need the intermediate variables, they detract from readability
    * in is_square?, don't need the return or the if as the result is what we wanted

--- lib/closest-fibonacci.rb
+++ lib/closest-fibonacci.rb
@@ -1,15 +1,13 @@
 Fixnum.class_eval do
   def closest_fibonacci
     self.downto 0 do |n|
-      nu = (5 * n**2 + 4)
-      nd = (5 * n**2 - 4)
-      if nu.is_square? or nd.is_square?
+      if (5 * n**2 - 4).is_square? or (5 * n**2 + 4).is_square?
         return n
       end
     end
   end  
 
   def is_square?
-    return true if Math.sqrt(self).floor**2 == self
+    Math.sqrt(self).floor**2 == self
   end
 end
